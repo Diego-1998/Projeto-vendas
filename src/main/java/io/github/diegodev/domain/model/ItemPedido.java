0a23796315ef3f42449eaffb356df7767a99ad66
Diego leandro <diegolgaspar000@gmail.com>
2022-02-28 22:48:24 -0300

    adicionando classe modelo ItemPedido no projeto

---
+++ src/main/java/io/github/diegodev/domain/model/ItemPedido.java
@@ -0,0 +1,9 @@
+package io.github.diegodev.domain.model;
+
+public class ItemPedido {
+
+    private Integer id;
+    private Pedido pedido;
+    private Produto produto;
+    private Integer quantidade;
+}
