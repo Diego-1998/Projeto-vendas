dadf2f3a6dd7a907af5cf61dfe5e0b4002fab8c8
Diego leandro <diegolgaspar000@gmail.com>
2022-03-09 22:47:42 -0300

    atualizando projeto

--- src/main/java/io/github/diegodev/domain/model/Cliente.java
+++ src/main/java/io/github/diegodev/domain/model/Cliente.java
@@ -1,4 +1,20 @@
 package io.github.diegodev.domain.model;
 
-public class Cliente {
+import javax.persistence.*;
+
+@Entity
+public class Cliente{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(length = 100)
+    private String nome;
+
+    public Cliente(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 }
---
+++ src/main/java/io/github/diegodev/domain/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package io.github.diegodev.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+
+
+}
